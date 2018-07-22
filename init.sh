6dfa2297f588370b32d32752b72ed7dbd15b0a78
Aaron Meier <aaron@0x41.ch>
2018-07-22 21:59:46 +0200

    Update init script

--- init.sh
+++ init.sh
@@ -11,20 +11,14 @@ stow tmux
 stow vim
 
 if [ "$(uname)" == 'Darwin' ]; then
-    # mac Only
     stow ansible
     stow beets
     stow cheat
-    stow chunkwm
     stow git
     stow gnupg
-    stow ideavim
-    stow khd
-    stow nvim
     stow postgres
     stow ssh
     stow vscode
-
     defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$HOME/.dotfiles/google/iterm"
     defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
 fi
